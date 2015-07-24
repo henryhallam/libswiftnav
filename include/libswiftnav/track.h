562a934b06b1e174ed6dad0344795124d937956a
Henry Hallam <henry@swiftnav.com>
2015-07-23 22:08:18 -0700

    TRACK: tdcp_doppler() accepts delta-time argument rather than calculating from time-of-transmit

--- include/libswiftnav/track.h
+++ include/libswiftnav/track.h
@@ -240,7 +240,8 @@ void calc_navigation_measurement_(u8 n_channels, channel_measurement_t* meas[],
 int nav_meas_cmp(const void *a, const void *b);
 u8 tdcp_doppler(u8 n_new, navigation_measurement_t *m_new,
                 u8 n_old, navigation_measurement_t *m_old,
-                navigation_measurement_t *m_corrected);
+                navigation_measurement_t *m_corrected,
+                double dt);
 
 #endif /* LIBSWIFTNAV_TRACK_H */
 
